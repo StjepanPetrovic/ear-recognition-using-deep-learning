08f3a5030a254a97de6d5fafbe665761aa6d332a
Stjepan Petrovic <stjepanpetrovic11@gmail.com>
2023-12-09 15:37:40 +0100

    Convert resized images to grayscale

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Ear recognition using deep learning
 
 ## Database selection and preparation:
-Ear images are downloaded from https://webctim.ulpgc.es/research_works/ami_ear_database/ and they are resized to 128 pixels in width.
+Ear images are downloaded from https://webctim.ulpgc.es/research_works/ami_ear_database/ and they are:
+- resized to 128 pixels in width,
+- resized images are converted to grayscale.
--- src/data/prepare_data.py
+++ src/data/prepare_data.py
@@ -31,6 +31,29 @@ def resize_images(raw_data_directory):
     print("Resizing is completed.")
 
 
+def convert_to_grayscale(resized_data_directory):
+    resized_ear_images = os.listdir(resized_data_directory)
+
+    for filename in tqdm(resized_ear_images):
+        img_path = os.path.join(resized_data_directory, filename)
+
+        grayscale_data_directory = '../../data/grayscale'
+        new_filename = filename.replace('.jpg', '_grayscale.jpg')
+        new_img_path = os.path.join(grayscale_data_directory, new_filename)
+
+        img = cv2.imread(img_path)
+
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        if not os.path.exists(grayscale_data_directory):
+            os.makedirs(grayscale_data_directory)
+
+        cv2.imwrite(new_img_path, gray_img)
+
+    print("Converting to grayscale is completed.")
+
+
 if __name__ == '__main__':
 
     resize_images('../../data/raw/')
+    convert_to_grayscale('../../data/resized/')
