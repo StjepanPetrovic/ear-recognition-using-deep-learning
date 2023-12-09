d979bc9bc9e18bc481bf3a713675be09eff6ec83
Stjepan Petrovic <stjepanpetrovic11@gmail.com>
2023-12-09 15:11:41 +0100

    Resize raw images to 128px width

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Ear recognition using deep learning
 
 ## Database selection and preparation:
-Ear images are downloaded from https://webctim.ulpgc.es/research_works/ami_ear_database/.
+Ear images are downloaded from https://webctim.ulpgc.es/research_works/ami_ear_database/ and they are resized to 128 pixels in width.
---
+++ src/data/prepare_data.py
@@ -0,0 +1,36 @@
+import os
+import cv2
+from tqdm import tqdm
+
+
+def resize_images(raw_data_directory):
+    raw_ear_images = os.listdir(raw_data_directory)
+
+    for filename in tqdm(raw_ear_images):
+        img_path = os.path.join(raw_data_directory, filename)
+
+        resized_data_directory = '../../data/resized'
+        new_filename = filename.replace('.jpg', '_resized.jpg')
+        new_img_path = os.path.join(resized_data_directory, new_filename)
+
+        img = cv2.imread(img_path)
+
+        h, w = img.shape[:2]
+        aspect_ratio = h / w
+
+        new_width = 128
+        new_height = int(new_width * aspect_ratio)
+
+        resized_img = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_AREA)
+
+        if not os.path.exists(resized_data_directory):
+            os.makedirs(resized_data_directory)
+
+        cv2.imwrite(new_img_path, resized_img)
+
+    print("Resizing is completed.")
+
+
+if __name__ == '__main__':
+
+    resize_images('../../data/raw/')
