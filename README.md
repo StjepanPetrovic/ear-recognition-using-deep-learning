aa973ef66967b80343bf98bc4c636b4f3f822fc6
Stjepan Petrovic <stjepanpetrovic11@gmail.com>
2023-12-10 00:15:19 +0100

    Add function for normalizing images, refactor code and rename folder

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 ## Database selection and preparation:
 Ear images are downloaded from https://webctim.ulpgc.es/research_works/ami_ear_database/ and they are:
 - resized to 128 pixels in width,
-- resized images are converted to grayscale.
+- resized images are converted to grayscale,
+- normalized to have values between 0 and 1 and saved as numpy arrays.
--- src/data/prepare_data.py
+++ src/data/prepare_data.py
@@ -1,17 +1,19 @@
 import os
 import cv2
+import numpy as np
 from tqdm import tqdm
 
 
 def resize_images(raw_data_directory):
     raw_ear_images = os.listdir(raw_data_directory)
 
+    resized_data_directory = '../../data/02_resized'
+
+    if not os.path.exists(resized_data_directory):
+        os.makedirs(resized_data_directory)
+
     for filename in tqdm(raw_ear_images):
         img_path = os.path.join(raw_data_directory, filename)
-
-        resized_data_directory = '../../data/resized'
-        new_filename = filename.replace('.jpg', '_resized.jpg')
-        new_img_path = os.path.join(resized_data_directory, new_filename)
 
         img = cv2.imread(img_path)
 
@@ -23,8 +25,7 @@ def resize_images(raw_data_directory):
 
         resized_img = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_AREA)
 
-        if not os.path.exists(resized_data_directory):
-            os.makedirs(resized_data_directory)
+        new_img_path = os.path.join(resized_data_directory, filename)
 
         cv2.imwrite(new_img_path, resized_img)
 
@@ -34,26 +35,51 @@ def resize_images(raw_data_directory):
 def convert_to_grayscale(resized_data_directory):
     resized_ear_images = os.listdir(resized_data_directory)
 
+    grayscale_data_directory = '../../data/03_grayscale'
+
+    if not os.path.exists(grayscale_data_directory):
+        os.makedirs(grayscale_data_directory)
+
     for filename in tqdm(resized_ear_images):
         img_path = os.path.join(resized_data_directory, filename)
-
-        grayscale_data_directory = '../../data/grayscale'
-        new_filename = filename.replace('.jpg', '_grayscale.jpg')
-        new_img_path = os.path.join(grayscale_data_directory, new_filename)
 
         img = cv2.imread(img_path)
 
         gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        if not os.path.exists(grayscale_data_directory):
-            os.makedirs(grayscale_data_directory)
+        new_img_path = os.path.join(grayscale_data_directory, filename)
 
         cv2.imwrite(new_img_path, gray_img)
 
     print("Converting to grayscale is completed.")
 
 
+def normalize_images(grayscale_data_directory):
+    grayscale_ear_images = os.listdir(grayscale_data_directory)
+
+    normalized_data_directory = '../../data/04_normalized'
+
+    if not os.path.exists(normalized_data_directory):
+        os.makedirs(normalized_data_directory)
+
+    for filename in tqdm(grayscale_ear_images):
+        img_path = os.path.join(grayscale_data_directory, filename)
+
+        filename = filename.split('.')[0] + '.npy'
+
+        new_img_path = os.path.join(normalized_data_directory, filename)
+
+        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+
+        normalized_img = cv2.normalize(img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+
+        np.save(new_img_path, normalized_img)
+
+    print("Normalizing is completed.")
+
+
 if __name__ == '__main__':
 
-    resize_images('../../data/raw/')
-    convert_to_grayscale('../../data/resized/')
+    resize_images('../../data/01_raw/')
+    convert_to_grayscale('../../data/02_resized/')
+    normalize_images('../../data/03_grayscale/')
