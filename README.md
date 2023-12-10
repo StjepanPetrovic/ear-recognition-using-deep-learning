62d18c3919a8802c3517238446d7eb0de91f87be
Stjepan Petrovic <stjepanpetrovic11@gmail.com>
2023-12-10 11:59:44 +0100

    Add method to split dataset into train, validation, and test set

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Ear recognition using deep learning
 
 ## Database selection and preparation:
-Ear images are downloaded from https://webctim.ulpgc.es/research_works/ami_ear_database/ and they are:
-- resized to 128 pixels in width,
-- resized images are converted to grayscale,
-- normalized to have values between 0 and 1 and saved as numpy arrays.
+Ear images are downloaded from https://webctim.ulpgc.es/research_works/ami_ear_database/ and stored in the `data/01_raw` folder.
+
+In terminal go to `src/data` directory and run `python3 prepare_data.py` to prepare the data for model. The script will:
+- resize images to 128 pixels in width and store it in `data/02_resized` directory,
+- resized images will convert to grayscale and stored in `data/03_grayscale` directory,
+- normalize to have values between 0 and 1 and saved as numpy arrays in `data/04_normalized` directory,
+- take grayscale images and create arrays of images and labels split for model to train, test and validate.
+
+## Setup project environment
--- src/data/prepare_data.py
+++ src/data/prepare_data.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import numpy as np
 from tqdm import tqdm
+from sklearn.model_selection import train_test_split
 
 
 def resize_images(raw_data_directory):
@@ -78,8 +79,37 @@ def normalize_images(grayscale_data_directory):
     print("Normalizing is completed.")
 
 
+def split_dataset(grayscale_data_directory):
+    grayscale_imgs = os.listdir(grayscale_data_directory)
+    grayscale_imgs.sort()
+
+    imgs_array = []
+    labels = []
+
+    for filename in tqdm(grayscale_imgs):
+        img_path = os.path.join(grayscale_data_directory, filename)
+
+        label = filename.split('_')[0]
+        labels.append(int(label))
+
+        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+
+        imgs_array.append(img)
+
+    imgs_array = np.array(imgs_array)
+    labels = np.array(labels)
+
+    img_train, img_temp, label_train, label_temp = train_test_split(imgs_array, labels, test_size=0.4, random_state=42)
+    img_val, img_test, label_val, label_test = train_test_split(img_temp, label_temp, test_size=0.5, random_state=42)
+
+    return (img_train, label_train), (img_val, label_val), (img_test, label_test)
+
+
 if __name__ == '__main__':
 
     resize_images('../../data/01_raw/')
     convert_to_grayscale('../../data/02_resized/')
     normalize_images('../../data/03_grayscale/')
+    ((img_training_set, label_training_set),
+     (img_validation_set, label_validation_set),
+     (img_test_set, label_test_set)) = split_dataset('../../data/03_grayscale/')
